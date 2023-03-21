6a97e6d7cd60108191c8d2d279108adad053d779
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-21 11:52:52 -0300

    Add files via upload

---
+++ Generics/Generics.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// Generics
+
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result: [Item] = []
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+
+makeArray(repeating: 2, numberOfTimes: 4)
+
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Element: Equatable, T.Element == U.Element
+{
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+   return false
+}
+anyCommonElements([1, 2, 3], [3])
