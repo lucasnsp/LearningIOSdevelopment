1ec525c42d6563abcf3faa1db796194c37cafad8
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-22 10:23:21 -0300

    Add files via upload

---
+++ Tipos de Dados/Tipos de Dados.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Tipos de Dados
+
+let int = 1 // Int -> Inteiros
+let double = 3.14 // Double -> números decimais
+let string = "Lucas" // String -> Textos
+let isDriver = true // Bool -> true ou false
+
+let number: Int = 2
+let pi: Double = 3.14
+let name: String = "Lucas"
+let no: Bool = false
