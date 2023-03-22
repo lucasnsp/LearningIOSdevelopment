cca9ddecff9e75909d14722864d2cd9e8d72aaf3
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-22 10:11:28 -0300

    Add files via upload

---
+++ variaveis/Variaveis.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// var é uma variável mutável para armazenar dados.
+var message = "Olá mundo"
+message = "Lucas Neves"
+
+// let (constante) é uma variável imutável para amarmazenar dados.
+let name = "Lucas"
+
+print(name)
+print(message)
+
+
+// comentario 1 linha
+
+// comentario em +1 linha
+
+/*
+ comentario de
+ multi linhas
+ */
