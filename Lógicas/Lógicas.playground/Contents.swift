40028f28da49bca86162be19d904826b3e997525
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-22 10:50:18 -0300

    Add files via upload

---
+++ Lógicas/Lógicas.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+// Incrementos, operadores Lógicos
+
+let result = 2 > 3
+
+let x = 10
+let y = 20
+
+let res = x == y // Comparação, para sair verdadeiro ou falso (x é igual a y?)
+let ress = x != y // Aqui pergutamos se x é diferente de y
+
+let firstName = "Tiago"
+let lastName = "Aguiar"
+
+firstName > lastName
+
+// == , != , = , < , <= , > , >=
+
+// ! && ||
+
+let isDriver = false
+let isStudent = true
+
+!isDriver
+
+// ! não (torna o falso em verdaeiro e o verdadeiro em falso.
+
+// ou || se uma das variaveis for verdadeiro o resultado será verdadeiro.
+
+//let r = isDriver || isStudent
+//print(r)
+
+// e && para que seja verdadeiro as duas condições tem que ser verdadeiras.
+
+let r = isDriver && isStudent
+print(r)
