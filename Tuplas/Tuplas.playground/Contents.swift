6bd21a344a48e6087f4e754ce5f8fe6cfb80f3d3
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-23 10:23:37 -0300

    Add files via upload

---
+++ Tuplas/Tuplas.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// Tuplas (coleções de dados)
+
+let latitude: Double = 23.21
+
+//let coords: (Double, Double) = (23.4, 54.22)
+//coords.0
+//coords.1
+
+let coords = (lat: 23.2, lng: 54.22)
+
+coords.lat
+coords.lng
+
+let camera = (x: 10, y: 20, z: 1)
+
+let (x, y, z) = camera
+
+print(x)
+print(y)
+print(z)
+print(camera)
+
+let user = (name: "Lucas", age: 22)
+
+user.name
+user.age
+
+print(user)
