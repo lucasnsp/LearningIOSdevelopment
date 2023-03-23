d8a3302911f9dcfcd6b5cbd2195384ce36f55950
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-23 10:40:20 -0300

    Add files via upload

---
+++ Arrays/Arrays.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// Arrays
+
+//let userNames: [String] = ["Tiago", "Jorge", "Cleiton"]
+
+var userNames: [String] = []
+
+userNames.append("Filipe")
+
+userNames += ["Steve", "Elon"]
