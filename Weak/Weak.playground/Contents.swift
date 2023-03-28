3e61052eb8fcda3a04b2ec80d3ea5bdbd0fa996a
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-28 09:31:47 -0300

    Add files via upload

---
+++ Weak/Weak.playground/Contents.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+// Weak
+
+// ARC (Automatic Reference Counting (libera a memoria que não está sendo utilizada.)
+// é um contador automatico de refencia que basicamente vai trabalhar para você para gerencia a memoria do seu app, e só vai desalocar espaços de memoria quando as refencias que voce criou os objetos que vc criou que você deu vida não tiver nenhum mais apontado para ele.
+
+
+
